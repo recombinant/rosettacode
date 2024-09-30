5b23c2cbfe24581e14f5496250990cf1cac82eb6
stephen <>
2024-09-30 12:44:04 +0000

    chore: O%27Halloran_numbers.zig add to repository
    FossilOrigin-Name: d405c42804a140b10a10f3c43cb7f43d97be25243c5fe4d599716b9d4440da14

---
+++ O%27Halloran_numbers.zig
@@ -0,0 +1,30 @@
+// https://rosettacode.org/wiki/O%27Halloran_numbers
+// Translated from C++
+const std = @import("std");
+
+pub fn main() !void {
+    const maximum_area = 1_000;
+    const half_maximum_area = maximum_area / 2;
+
+    var ohalloran_numbers = std.StaticBitSet(half_maximum_area).initFull();
+    for (0..3) |i|
+        ohalloran_numbers.unset(i);
+
+    for (1..maximum_area) |length|
+        for (1..half_maximum_area) |width|
+            for (1..half_maximum_area) |height| {
+                const half_area = length * width + length * height + width * height;
+                if (half_area < half_maximum_area)
+                    ohalloran_numbers.unset(half_area);
+            };
+
+    const stdout = std.io.getStdOut().writer();
+    try stdout.print(
+        "{} even integer values larger than 6 and less than 1000 which cannot be the surface area of an integer cuboid:\n",
+        .{ohalloran_numbers.count()},
+    );
+    for (3..half_maximum_area) |i|
+        if (ohalloran_numbers.isSet(i))
+            try stdout.print("{} ", .{i * 2});
+    try stdout.writeByte('\n');
+}
