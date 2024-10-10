f209f4a1ec11a4ed9334f8ad8cb4f08f9e5f1319
stephen <>
2024-10-10 19:28:36 +0000

    add: Rot-13.zig
    FossilOrigin-Name: 28d92837c3d71b24c72e8cd5c515ce880460b4965e110880f3ad08dc940e5991

---
+++ Rot-13.zig
@@ -0,0 +1,29 @@
+// https://rosettacode.org/wiki/Rot-13
+
+pub fn main() void {
+    const message: []const u8 =
+        \\Lbh xabj vg vf tbvat gb or n onq qnl
+        \\jura gur yrggref va lbhe nycunorg fbhc
+        \\fcryy Q-V-F-N-F-G-R-E.
+    ;
+    // This works because constant 'message' is comptime known
+    var buffer: [message.len]u8 = undefined;
+
+    // Copy constant 'message' to variable 'buffer' for later modification in-place
+    @memcpy(&buffer, message);
+    defer @memset(&buffer, 0); // Security: overwrite 'buffer' after use
+
+    @import("std").debug.print("rot13={s}\n", .{rot13(&buffer)});
+}
+
+/// Modifies parameter 'slice' in-place
+fn rot13(slice: []u8) []u8 {
+    for (slice) |*c| {
+        c.* = switch (c.*) {
+            'A'...'Z' - 13, 'a'...'z' - 13 => |n| n + 13,
+            'Z' - 12...'Z', 'z' - 12...'z' => |n| n - 26 + 13,
+            else => |n| n,
+        };
+    }
+    return slice;
+}
