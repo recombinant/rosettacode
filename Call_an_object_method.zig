0dc59705b036b8dfbc1d5963c0d72504dbfa0fd4
stephen <>
2024-10-14 17:46:59 +0000

    add: Call_an_object_method.zig (originated from rosettacode)
    FossilOrigin-Name: 8d8616f5d56fea3429f99fb2671eb857b24861b6dc2e4d99dd64b777ea0be0ef

---
+++ Call_an_object_method.zig
@@ -0,0 +1,36 @@
+// https://rosettacode.org/wiki/Call_an_object_method
+// Copied from rosettacode
+const testing = @import("std").testing;
+
+pub const ID = struct {
+    name: []const u8,
+    age: u7,
+
+    const Self = @This();
+
+    pub fn init(name: []const u8, age: u7) Self {
+        return Self{
+            .name = name,
+            .age = age,
+        };
+    }
+
+    pub fn getAge(self: Self) u7 {
+        return self.age;
+    }
+};
+
+test "call an object method" {
+    // Declare an instance of a struct by using a struct method.
+    const person1 = ID.init("Alice", 18);
+
+    // Or by declaring it manually.
+    const person2 = ID{
+        .name = "Bob",
+        .age = 20,
+    };
+
+    // test getAge() method call
+    try testing.expectEqual(@as(u7, 18), person1.getAge());
+    try testing.expectEqual(@as(u7, 20), ID.getAge(person2));
+}
