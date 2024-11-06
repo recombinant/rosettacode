a4fe466306c13cb3fd321674e1db8b9d4b8b94f6
stephen <>
2024-11-06 19:22:43 +0000

    fix: minor correction to isPrime() function
    FossilOrigin-Name: e9597b0083131d257dc64e06ce01180c5ed396a8c852d52087d5f7593a45de54

--- Ulam_spiral_(for_primes).zig
+++ Ulam_spiral_(for_primes).zig
@@ -50,7 +50,7 @@ fn isPrime(n: anytype) bool {
 
     const wheel = [_]T{ 4, 2, 4, 2, 4, 6, 2, 6 };
 
-    var p: u64 = 7;
+    var p: T = 7;
     while (true) {
         for (wheel) |w| {
             if (p * p > n)
