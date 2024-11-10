76de5daa9b72ab0e5b248fb69899f2f6757b5463
stephen <>
2024-11-10 09:42:33 +0000

    style: Rename array constants Loop_over_multiple_arrays_simultaneously.zig
    FossilOrigin-Name: 4cdce3f992fb401a8426771f3a51e1aba1fe63b491d299caad6169616acee7fc

--- Loop_over_multiple_arrays_simultaneously.zig
+++ Loop_over_multiple_arrays_simultaneously.zig
@@ -2,10 +2,10 @@
 const std = @import("std");
 
 pub fn main() void {
-    const a1 = [3]u8{ 'a', 'b', 'c' };
-    const a2 = [3]u8{ 'A', 'B', 'C' };
-    const a3 = [3]u32{ 1, 2, 3 };
+    const array1 = [3]u8{ 'a', 'b', 'c' };
+    const array2 = [3]u8{ 'A', 'B', 'C' };
+    const array3 = [3]u32{ 1, 2, 3 };
 
-    for (a1, a2, a3) |v1, v2, v3|
+    for (array1, array2, array3) |v1, v2, v3|
         std.debug.print("{c}{c}{}\n", .{ v1, v2, v3 });
 }
