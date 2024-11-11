033e715b33702b329085cb96322cd343880d7968
stephen <>
2024-11-11 13:05:52 +0000

    add: Selectively_replace_multiple_instances_of_a_character_within_a_string.zig
    FossilOrigin-Name: f7e0814df37c04f451f3892bd580fb374ef4dd166d4939adb9d5ad4b4ddc9db3

---
+++ Selectively_replace_multiple_instances_of_a_character_within_a_string.zig
@@ -0,0 +1,34 @@
+// https://rosettacode.org/wiki/Selectively_replace_multiple_instances_of_a_character_within_a_string
+// Translation of C
+const std = @import("std");
+
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
+
+    const string = "abracadabra";
+
+    const replaced = try allocator.dupe(u8, string);
+    defer allocator.free(replaced);
+
+    // Null terminated replacement character arrays
+    var a_rep: [*c]const u8 = "ABaCD";
+    var b_rep: [*c]const u8 = "E";
+    var r_rep: [*c]const u8 = "rF";
+
+    for (replaced) |*ch| {
+        const ptr: *[*c]const u8 = switch (ch.*) {
+            'a' => &a_rep,
+            'b' => &b_rep,
+            'r' => &r_rep,
+            else => continue,
+        };
+        // Use C-style pointer arithmetic
+        if (ptr.*.* != 0) {
+            ch.* = ptr.*.*;
+            ptr.* += 1;
+        }
+    }
+    std.debug.print("{s}\n", .{replaced});
+}
