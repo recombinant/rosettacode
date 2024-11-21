54fc38b7d670ed16408a96b735979327c225fd1e
stephen <>
2024-11-21 10:33:48 +0000

    add: Sum_and_product_of_an_array.zig
    FossilOrigin-Name: ca26a587ff882c4bb671b9a08d6f00a2568ace9e52d632612493d7357e1c6ef5

---
+++ Sum_and_product_of_an_array.zig
@@ -0,0 +1,13 @@
+// https://rosettacode.org/wiki/Sum_and_product_of_an_array
+// Copied from rosettacode
+const print = @import("std").debug.print;
+pub fn main() void {
+    const numbers = [_]u8{ 1, 2, 3, 4, 5 };
+    var sum: u8 = 0;
+    var product: u8 = 1;
+    for (numbers) |number| {
+        product *= number;
+        sum += number;
+    }
+    print("{} {}\n", .{ product, sum });
+}
