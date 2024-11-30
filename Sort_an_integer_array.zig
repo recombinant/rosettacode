9fe6a161b4841f62cb7c3b505dae1d0b9aaf2d4a
stephen <>
2024-11-30 19:08:25 +0000

    add: Sort_an_integer_array.zig
    FossilOrigin-Name: 01b36c717eca1389a3d641e19ee5d03de69c305556504ad3ac843c4a38db16bf

---
+++ Sort_an_integer_array.zig
@@ -0,0 +1,9 @@
+// https://rosettacode.org/wiki/Sort_an_integer_array
+const std = @import("std");
+
+pub fn main() void {
+    var nums = [_]c_int{ 6, 2, 7, 8, 3, 1, 10, 5, 4, 9 };
+    std.mem.sort(c_int, &nums, {}, std.sort.asc(c_int));
+
+    std.debug.print("{any}\n", .{nums});
+}
