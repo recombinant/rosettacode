af23b59a71e9935070afa3c30392f07a7659aa87
stephen <>
2024-12-04 22:14:49 +0000

    add: Dot_product.zig
    FossilOrigin-Name: 86c46bda53c55be41f6315d453d71ab75e83e6742ec1e6f838aaf04664aab40b

---
+++ Dot_product.zig
@@ -0,0 +1,11 @@
+// https://rosettacode.org/wiki/Dot_product
+// Copied from rosettacode
+const std = @import("std");
+
+pub fn main() !void {
+    const a = @Vector(3, i32){ 1, 3, -5 };
+    const b = @Vector(3, i32){ 4, -2, -1 };
+    const dot: i32 = @reduce(.Add, a * b);
+
+    try std.io.getStdOut().writer().print("{d}\n", .{dot});
+}
