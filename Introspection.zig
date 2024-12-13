02ad3dfbdad25b526c3ffd0bde58de5e7d2119fe
stephen <>
2024-12-13 09:25:16 +0000

    add: Introspection.zig & Introspection_extra_credit.zig
    FossilOrigin-Name: 10d7fe4af4bc80b87a617f8911f869a47f363a625e7dfb62ea9ec40ea7edfd0f

---
+++ Introspection.zig
@@ -0,0 +1,26 @@
+// https://rosettacode.org/wiki/Introspection
+// Copied from rosettacode
+// Works with: 0.10.1, 0.11.0, 0.12.0, 0.12.1, 0.13.0, 0.14dev
+const std = @import("std");
+const builtin = @import("builtin");
+
+pub const bloop: i32 = -1_000;
+
+pub fn abs(a: i32) i32 {
+    return if (a < 0) -a else a;
+}
+
+pub fn main() error{NotSupported}!void {
+    if (builtin.zig_version.order(.{ .major = 0, .minor = 12, .patch = 1 }) == .lt) {
+        std.debug.print("Version {any} is less than 0.11.0, not suitable, exiting!\n", .{builtin.zig_version});
+        return error.NotSupported;
+    } else {
+        std.debug.print("Version {any} is more or equal than 0.12.1, suitable, continuing!\n", .{builtin.zig_version});
+    }
+
+    if (@hasDecl(@This(), "bloop") and @hasDecl(@This(), "abs")) {
+        std.debug.print("abs(bloop) = {d}\n", .{abs(bloop)});
+    } else {
+        std.debug.print("abs and/or bloop are not defined!\n", .{});
+    }
+}
---
+++ Introspection_extra_credit.zig
@@ -0,0 +1,41 @@
+// https://rosettacode.org/wiki/Introspection
+// Copied from rosettacode
+// Extra Credit
+// Works with: 0.10.1, 0.11.0, 0.12.0, 0.12.1, 0.13.0, 0.14dev
+const std = @import("std");
+const builtin = @import("builtin");
+
+pub const first_integer_constant: i32 = 5;
+pub const second_integer_constant: i32 = 3;
+pub const third_integer_constant: i32 = -2;
+
+pub const some_non_integer_constant: f32 = 0.0;
+pub const another_non_integer_constant: bool = false;
+
+pub fn main() void {
+    comptime var cnt: comptime_int = 0;
+    comptime var sum: comptime_int = 0;
+    if (comptime builtin.zig_version.order(.{ .major = 0, .minor = 13, .patch = 0 }) == .gt) {
+        inline for (@typeInfo(@This()).@"struct".decls) |decl_info| {
+            const decl = @field(@This(), decl_info.name);
+            switch (@typeInfo(@TypeOf(decl))) {
+                .int, .comptime_int => {
+                    sum += decl;
+                    cnt += 1;
+                },
+                else => continue,
+            }
+        }
+    } else inline for (@typeInfo(@This()).Struct.decls) |decl_info| {
+        const decl = @field(@This(), decl_info.name);
+        switch (@typeInfo(@TypeOf(decl))) {
+            .Int, .ComptimeInt => {
+                sum += decl;
+                cnt += 1;
+            },
+            else => continue,
+        }
+    }
+
+    @compileLog(std.fmt.comptimePrint("cnt = {d}, sum = {d}", .{ cnt, sum }));
+}
