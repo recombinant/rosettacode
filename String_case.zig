6756fae4fb6b254153c63fba02f517b9ebb0dccf
stephen <>
2024-12-13 21:25:58 +0000

    add: String_case.zig
    FossilOrigin-Name: 467c42f1a099400255fbd328a8a9bce86bbe12adb87b6f7a94a5bb04d3ae3ed2

---
+++ String_case.zig
@@ -0,0 +1,18 @@
+// https://rosettacode.org/wiki/String_case
+// Copied from rosettacode
+const std = @import("std");
+
+pub fn main() !void {
+    const stdout_wr = std.io.getStdOut().writer();
+    const string = "alphaBETA";
+    var lower: [string.len]u8 = undefined;
+    var upper: [string.len]u8 = undefined;
+    for (string, &lower, &upper) |char, *lo, *up| {
+        lo.* = std.ascii.toLower(char);
+        up.* = std.ascii.toUpper(char);
+    }
+    try stdout_wr.print("lower: {s}\n", .{lower});
+    try stdout_wr.print("upper: {s}\n", .{upper});
+
+    // TODO use https://codeberg.org/dude_the_builder/zigstr
+}
