2222b9395714fb932ef2691fced332a7704a51d9
stephen <>
2024-12-13 14:54:01 +0000

    add: Determine_sentence_type.zig
    FossilOrigin-Name: e25c548fddcb21020b00b4e0591972a4151fd81ef2dba61fbdc5c91f92bb7328

---
+++ Determine_sentence_type.zig
@@ -0,0 +1,24 @@
+// https://rosettacode.org/wiki/Determine_sentence_type
+const std = @import("std");
+
+pub fn main() void {
+    const paragraph = "hi there, how are you today? I'd like to present to you the washing machine 9001. You have been nominated to win one of these! Just make sure you don't break it";
+    const fmt = "{s:<52} ==> {c}\n";
+    var pos0: usize = 0;
+    while (std.mem.indexOfAnyPos(u8, paragraph, pos0, "?!.")) |pos1| {
+        const ch: u8 = switch (paragraph[pos1]) {
+            '?' => 'Q',
+            '!' => 'E',
+            '.' => 'S',
+            else => unreachable,
+        };
+        const sentence = std.mem.trimLeft(u8, paragraph[pos0 .. pos1 + 1], " ");
+        std.debug.print(fmt, .{ sentence, ch });
+        pos0 = pos1 + 1;
+    }
+    if (pos0 != paragraph.len) {
+        const ch: u8 = 'N';
+        const sentence = std.mem.trimLeft(u8, paragraph[pos0..paragraph.len], " ");
+        std.debug.print(fmt, .{ sentence, ch });
+    }
+}
