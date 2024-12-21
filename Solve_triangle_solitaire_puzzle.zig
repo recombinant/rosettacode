f151ce08ae9e789a85e4f6346b3a4b76dfe0d7a3
stephen <>
2024-12-21 16:48:02 +0000

    add: Solve_triangle_solitaire_puzzle.zig
    FossilOrigin-Name: bd9ceea1a11beaee395e8d0a4b3751672bc780e530f6963459673d754915c73f

---
+++ Solve_triangle_solitaire_puzzle.zig
@@ -0,0 +1,131 @@
+// https://rosettacode.org/wiki/Solve_triangle_solitaire_puzzle
+// Translation of Kotlin
+const std = @import("std");
+
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
+
+    const writer = std.io.getStdOut().writer();
+
+    var board = Board.init();
+
+    var solutions = std.ArrayList(Solution).init(allocator);
+    defer solutions.deinit();
+
+    // run once to get solutions
+    const empty_start = 1;
+    board.start(empty_start); // starting with peg 1 removed
+    try board.solve(&solutions);
+    std.debug.assert(board.isSolved());
+
+    // restart & replay solutions printing the board
+    board.start(empty_start);
+    try board.drawBoard(writer);
+    try writer.print("Starting with peg {X} removed\n", .{empty_start});
+    for (solutions.items) |solution| {
+        board.replayMove(solution);
+        try writer.writeByte('\n');
+        try board.drawBoard(writer);
+        try writer.print(
+            "Peg {X} jumped over {X} to land on {X}\n",
+            .{ solution.peg, solution.over, solution.land },
+        );
+    }
+}
+
+const Solution = struct {
+    peg: u5,
+    over: u5,
+    land: u5,
+    fn init(peg: u5, over: u5, land: u5) Solution {
+        return Solution{ .peg = peg, .over = over, .land = land };
+    }
+};
+const Board = struct {
+    board: [16]bool,
+
+    const jump_moves: []const []const [2]u5 = &[_][]const [2]u5{
+        &[_][2]u5{},
+        &[_][2]u5{ .{ 2, 4 }, .{ 3, 6 } },
+        &[_][2]u5{ .{ 4, 7 }, .{ 5, 9 } },
+        &[_][2]u5{ .{ 5, 8 }, .{ 6, 10 } },
+        &[_][2]u5{ .{ 2, 1 }, .{ 5, 6 }, .{ 7, 11 }, .{ 8, 13 } },
+        &[_][2]u5{ .{ 8, 12 }, .{ 9, 14 } },
+        &[_][2]u5{ .{ 3, 1 }, .{ 5, 4 }, .{ 9, 13 }, .{ 10, 15 } },
+        &[_][2]u5{ .{ 4, 2 }, .{ 8, 9 } },
+        &[_][2]u5{ .{ 5, 3 }, .{ 9, 10 } },
+        &[_][2]u5{ .{ 5, 2 }, .{ 8, 7 } },
+        &[_][2]u5{.{ 9, 8 }},
+        &[_][2]u5{.{ 12, 13 }},
+        &[_][2]u5{ .{ 8, 5 }, .{ 13, 14 } },
+        &[_][2]u5{ .{ 8, 4 }, .{ 9, 6 }, .{ 12, 11 }, .{ 14, 15 } },
+        &[_][2]u5{ .{ 9, 5 }, .{ 13, 12 } },
+        &[_][2]u5{ .{ 10, 6 }, .{ 14, 13 } },
+    };
+
+    fn init() Board {
+        var b = Board{ .board = undefined };
+        b.clear();
+        return b;
+    }
+    fn clear(self: *Board) void {
+        @memset(&self.board, true);
+        self.board[0] = false; // board[0] is not used
+    }
+    fn start(self: *Board, empty_start: u5) void {
+        self.clear();
+        self.board[empty_start] = false;
+    }
+    fn replayMove(self: *Board, solution: Solution) void {
+        self.board[solution.peg] = false;
+        self.board[solution.over] = false;
+        self.board[solution.land] = true;
+    }
+    /// Recursive function to solve the puzzle
+    fn solve(self: *Board, solutions: *std.ArrayList(Solution)) !void {
+        if (isSolved(self))
+            return;
+        for (1..self.board.len) |peg| {
+            if (self.board[peg]) {
+                for (jump_moves[peg]) |ol| {
+                    const over, const land = ol;
+                    if (self.board[over] and !self.board[land]) {
+                        const save_board = self.board;
+                        self.board[peg] = false;
+                        self.board[over] = false;
+                        self.board[land] = true;
+                        try solutions.append(Solution.init(@truncate(peg), over, land));
+                        try solve(self, solutions);
+                        if (self.isSolved())
+                            return;
+                        // otherwise back-track
+                        self.board = save_board;
+                        _ = solutions.pop();
+                    }
+                }
+            }
+        }
+    }
+    fn isSolved(self: *const Board) bool {
+        var count: usize = 0;
+        for (self.board) |peg|
+            count += @intFromBool(peg);
+        return count == 1; // just one peg left
+    }
+    fn drawBoard(self: *const Board, writer: anytype) !void {
+        var pegs: [16]u8 = undefined;
+        @memset(&pegs, '-');
+        for (self.board, &pegs, 0..) |peg, *s, i|
+            if (peg) {
+                s.* = if (i < 10) '0' else 'A' - 10;
+                s.* += @truncate(i);
+            };
+        try writer.print("       {c}\n", .{pegs[1]});
+        try writer.print("      {c} {c}\n", .{ pegs[2], pegs[3] });
+        try writer.print("     {c} {c} {c}\n", .{ pegs[4], pegs[5], pegs[6] });
+        try writer.print("    {c} {c} {c} {c}\n", .{ pegs[7], pegs[8], pegs[9], pegs[10] });
+        try writer.print("   {c} {c} {c} {c} {c}\n", .{ pegs[11], pegs[12], pegs[13], pegs[14], pegs[15] });
+    }
+};
