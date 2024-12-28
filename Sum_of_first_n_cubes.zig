5798785540e897c5f44ca665b623d803841b76e0
stephen <>
2024-12-28 21:00:10 +0000

    add: Sum_of_first_n_cubes.zig
    FossilOrigin-Name: 8a452ad7ea3342666614f87fa747fe048389ac573c2c4223ebbf1e6ff922be1d

---
+++ Sum_of_first_n_cubes.zig
@@ -0,0 +1,15 @@
+// https://rosettacode.org/wiki/Sum_of_first_n_cubes
+// Copied from rosettacode
+
+pub fn main() !void {
+    const stdout = @import("std").io.getStdOut().writer();
+
+    var sum: u32 = 0;
+    var i: u32 = 0;
+
+    while (i < 50) : (i += 1) {
+        sum += i * i * i;
+        try stdout.print("{d:8}", .{sum});
+        if (i % 5 == 4) try stdout.print("\n", .{});
+    }
+}
