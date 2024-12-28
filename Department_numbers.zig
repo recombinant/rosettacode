3427054d16479c5903db5264463ea3c43d58ce76
stephen <>
2024-12-28 23:20:58 +0000

    add: Department_numbers.zig
    FossilOrigin-Name: c0cda8297a465b32a5efc6bceae30ff32f988788cf20bd1c714d6d8373f74395

---
+++ Department_numbers.zig
@@ -0,0 +1,16 @@
+// https://rosettacode.org/wiki/Department_numbers#Zig
+const std = @import("std");
+pub fn main() !void {
+    const stdout = std.io.getStdOut().writer();
+
+    try stdout.writeAll("Police  Sanitation  Fire\n");
+    try stdout.writeAll("------  ----------  ----\n");
+
+    var p: usize = 2;
+    while (p <= 7) : (p += 2)
+        for (1..7 + 1) |s|
+            for (1..7 + 1) |f|
+                if (p != s and s != f and f != p and p + f + s == 12) {
+                    try stdout.print("  {d}         {d}         {d}\n", .{ p, s, f });
+                };
+}
