77287539e0f4f8fcaa8af234a7da1f0448806299
stephen <>
2024-12-30 11:12:56 +0000

    refactor: Miller–Rabin_primality_test.zig to work with arbitrary bit-width unsigned integers. Minimum is u6 to test for primes <= 31
    FossilOrigin-Name: 7c67f82f924e8b5c1dc6c1db42c5ed88188cd55289522bffc5d80beb6428f154

--- Miller–Rabin_primality_test.zig
+++ Miller–Rabin_primality_test.zig
@@ -12,16 +12,23 @@ pub fn main() !void {
     });
     const random = prng.random();
     // ----------------------------------------------------------
-    std.debug.print("{}\n", .{primeTest(5127821565631733, random, .{})});
+    std.debug.print("{} (expected true)\n", .{primeTest(@as(u64, 5127821565631733), random, .{})});
+
     // https://t5k.org/lists/2small/0bit.html
-    std.debug.print("{}\n", .{primeTest(std.math.pow(u64, 2, 63) - 471, random, .{})});
+    std.debug.print("{} (expected true)\n", .{primeTest(std.math.pow(u64, 2, 63) - 471, random, .{})});
+
+    // https://t5k.org/lists/2small/300bit.html
+    std.debug.print("{} (expected true)\n", .{primeTest(std.math.pow(u401, 2, 400) - 593, random, .{})});
+
+    // https://t5k.org/lists/2small/300bit.html
+    std.debug.print("{} (expected false)\n", .{primeTest(std.math.pow(u401, 2, 400) - 591, random, .{})});
 }
 
 /// Compute a*b % mod
-fn modmult(a_: u64, b_: u64, mod: u64) u64 {
+fn modmult(T: type, a_: T, b_: T, mod: T) T {
     var a = a_;
     var b = b_;
-    var result: u64 = 0;
+    var result: T = 0;
     while (b != 0) {
         if (b % 2 == 1)
             result = (result + a) % mod;
@@ -32,14 +39,14 @@ fn modmult(a_: u64, b_: u64, mod: u64) u64 {
 }
 
 /// Compute a^b % mod
-fn modpow(a_: u64, b_: u64, mod: u64) u64 {
+fn modpow(T: type, a_: T, b_: T, mod: T) T {
     var a = a_;
     var b = b_;
-    var result: u64 = 1;
+    var result: T = 1;
     while (b != 0) {
         if (b % 2 == 1)
-            result = modmult(result, a, mod);
-        a = modmult(a, a, mod);
+            result = modmult(T, result, a, mod);
+        a = modmult(T, a, a, mod);
         b /= 2;
     }
     return result;
@@ -50,8 +57,14 @@ const PrimeTestOptions = struct {
 };
 
 /// Miller–Rabin primality test.
-fn primeTest(n: u64, random: std.Random, options: PrimeTestOptions) bool {
-    const primes = [_]u64{ 2, 3, 5, 7, 11, 13, 17, 19, 23 };
+fn primeTest(n: anytype, random: std.Random, options: PrimeTestOptions) bool {
+    const T = @TypeOf(n);
+    if (@typeInfo(T) != .int or @typeInfo(T).int.signedness != .unsigned)
+        @compileError("primeTest() expected unsigned integer argument, found " ++ @typeName(T));
+    if (@typeInfo(T).int.bits < 6)
+        @compileError("primeTest() expected minimum 6 bit argument, found " ++ @typeName(T));
+
+    const primes = [_]T{ 2, 3, 5, 7, 11, 13, 17, 19, 23 };
     for (primes) |p|
         if (n % p == 0)
             return n == p;
@@ -65,10 +78,10 @@ fn primeTest(n: u64, random: std.Random, options: PrimeTestOptions) bool {
         d /= 2;
     }
     for (0..options.num_tests) |_| {
-        const a = random.intRangeAtMost(u64, 2, n - 2);
-        var x = modpow(a, d, n);
+        const a = random.intRangeAtMost(T, 2, n - 2);
+        var x = modpow(T, a, d, n);
         for (0..s) |_| {
-            const y = modmult(x, x, n);
+            const y = modmult(T, x, x, n);
             if (y == 1 and x != 1 and x != n - 1) {
                 // Nontrivial square root of 1 modulo n
                 // (x+1)(x-1) divisible by n, meaning gcd(x+1, n) is a factor of n, negating primality
@@ -88,7 +101,7 @@ test "primeTest primes" {
     var prng = std.Random.DefaultPrng.init(testing.random_seed);
     const random = prng.random();
 
-    const primes = [_]u64{ 2, 3, 5, 7, 11, 13, 17, 19, 23, 29 };
+    const primes = [_]u7{ 2, 3, 5, 7, 11, 13, 17, 19, 23, 29 };
     for (primes) |p|
         try testing.expect(primeTest(p, random, .{}));
 }
@@ -97,10 +110,10 @@ test "primeTest composites" {
     var prng = std.Random.DefaultPrng.init(testing.random_seed);
     const random = prng.random();
 
-    const primes = [_]u64{ 2, 3, 5, 7, 11, 13, 17, 19, 23, 29 };
-    var i: u64 = 1;
+    const primes = [_]u7{ 2, 3, 5, 7, 11, 13, 17, 19, 23, 29 };
+    var i: u7 = 1;
     while (i < 31) : (i += 1)
-        if (std.mem.indexOfScalar(u64, &primes, i) == null)
+        if (std.mem.indexOfScalar(u7, &primes, i) == null)
             try testing.expect(!primeTest(i, random, .{}));
 }
 
@@ -108,27 +121,42 @@ test "primeTest large primes" {
     var prng = std.Random.DefaultPrng.init(testing.random_seed);
     const random = prng.random();
 
-    const n = std.math.pow(u64, 2, 62);
+    const n64 = std.math.pow(u64, 2, 62);
     // https://t5k.org/lists/2small/0bit.html
-    var primes = [_]u64{ 57, 87, 117, 143, 153, 167, 171, 195, 203, 273 };
-    for (&primes) |*p| {
-        p.* = n - p.*;
-    }
-    for (primes) |p| try testing.expect(primeTest(p, random, .{}));
+    var primes64 = [_]u64{ 57, 87, 117, 143, 153, 167, 171, 195, 203, 273 };
+    for (&primes64) |*p|
+        p.* = n64 - p.*;
+    for (primes64) |p| try testing.expect(primeTest(p, random, .{}));
+
+    const n128 = std.math.pow(u128, 2, 126);
+    // https://t5k.org/lists/2small/100bit.html
+    var primes128 = [_]u128{ 137, 203, 237, 261, 335, 341, 465, 663, 671, 783 };
+    for (&primes128) |*p|
+        p.* = n128 - p.*;
+    for (primes128) |p| try testing.expect(primeTest(p, random, .{}));
 }
 
 test "primeTest large composites" {
     var prng = std.Random.DefaultPrng.init(testing.random_seed);
     const random = prng.random();
 
-    const n = std.math.pow(u64, 2, 62);
+    const n64 = std.math.pow(u64, 2, 62);
     // https://t5k.org/lists/2small/0bit.html
-    var primes = [_]u64{ 57, 87, 117, 143, 153, 167, 171, 195, 203, 273 };
-    for (&primes) |*p| {
-        p.* = n - p.*;
-    }
-    var i = primes[primes.len - 1];
-    while (i <= n) : (i += 1)
-        if (std.mem.indexOfScalar(u64, &primes, i) == null)
+    var primes64 = [_]u64{ 57, 87, 117, 143, 153, 167, 171, 195, 203, 273 };
+    for (&primes64) |*p|
+        p.* = n64 - p.*;
+    var i = primes64[primes64.len - 1];
+    while (i <= n64) : (i += 1)
+        if (std.mem.indexOfScalar(u64, &primes64, i) == null)
             try testing.expect(!primeTest(i, random, .{}));
+
+    const n128 = std.math.pow(u127, 2, 126);
+    // https://t5k.org/lists/2small/100bit.html
+    var primes128 = [_]u128{ 137, 203, 237, 261, 335, 341, 465, 663, 671, 783 };
+    for (&primes128) |*p|
+        p.* = n128 - p.*;
+    var j = primes128[primes128.len - 1];
+    while (j <= n128) : (j += 1)
+        if (std.mem.indexOfScalar(u128, &primes128, j) == null)
+            try testing.expect(!primeTest(j, random, .{}));
 }
