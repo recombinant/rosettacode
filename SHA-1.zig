fbcf659aac8c17e4e57ecee95ac191267c3683d4
stephen <>
2024-12-31 22:31:21 +0000

    feat: SHA-1.zig append another example hash
    FossilOrigin-Name: 1c4fa9d65af47a036dac9abeb295349435555c598b63f46f361dcc74c42d801b

--- SHA-1.zig
+++ SHA-1.zig
@@ -3,10 +3,19 @@ const std = @import("std");
 const Sha1 = std.crypto.hash.Sha1;
 
 pub fn main() void {
-    const s = "Rosetta Code";
     var digest: [Sha1.digest_length]u8 = undefined;
-    Sha1.hash(s, digest[0..], .{});
 
-    std.debug.print("    String : {s}\n", .{s});
+    const s1 = "Rosetta code";
+    Sha1.hash(s1, digest[0..], .{});
+
+    std.debug.print("    String : {s}\n", .{s1});
+    std.debug.print("SHA-1 Hash : {s}\n", .{std.fmt.bytesToHex(digest, .lower)});
+
+    std.debug.print("\n", .{});
+
+    const s2 = "Rosetta Code";
+    Sha1.hash(s2, digest[0..], .{});
+
+    std.debug.print("    String : {s}\n", .{s2});
     std.debug.print("SHA-1 Hash : {s}\n", .{std.fmt.bytesToHex(digest, .lower)});
 }
