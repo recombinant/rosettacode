1b7fc4a9ab2fd86cf279f2bbc513af85ea9402e9
stephen <>
2024-12-31 22:34:22 +0000

    add: SHA-256.zig
    FossilOrigin-Name: 9231b9ada157714972e63d9c0fcc4959e9debf7107f08e6a57ad567a5a7dcfa4

---
+++ SHA-256.zig
@@ -0,0 +1,13 @@
+// https://rosettacode.org/wiki/SHA-256
+const std = @import("std");
+const Sha256 = std.crypto.hash.sha2.Sha256;
+
+pub fn main() void {
+    var digest: [Sha256.digest_length]u8 = undefined;
+
+    const s = "Rosetta code";
+    Sha256.hash(s, digest[0..], .{});
+
+    std.debug.print("      String : {s}\n", .{s});
+    std.debug.print("SHA-256 Hash : {s}\n", .{std.fmt.bytesToHex(digest, .lower)});
+}
