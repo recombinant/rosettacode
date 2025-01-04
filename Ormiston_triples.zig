adda6632fcb56207ec1d6a636139d675f446c69c
stephen <>
2025-01-04 00:00:53 +0000

    refactor: Ormiston_triples.zig using cpp primesieve from https://github.com/kimwalisch/primesieve/ to improve speed by more than one order of magnitude.
    FossilOrigin-Name: 2bbc65afdba108bdbc2c1ec2023c0185086cad710ce773209fce3e0e15938d65

--- Ormiston_triples.zig
+++ Ormiston_triples.zig
@@ -1,130 +1,112 @@
 // https://rosettacode.org/wiki/Ormiston_triples
 // Translation of C++
 const std = @import("std");
-const fmt = std.fmt;
-const heap = std.heap;
-const io = std.io;
 const mem = std.mem;
-const time = std.time;
-
-// https://rosettacode.org/wiki/Extensible_prime_generator
-const AutoSieveType = @import("sieve.zig").AutoSieveType;
-const PrimeGen = @import("sieve.zig").PrimeGen;
+// Using cpp primesieve from https://github.com/kimwalisch/primesieve/
+const ps = @cImport({
+    @cInclude("primesieve.h");
+});
 
 pub fn main() !void {
-    var t0 = try time.Timer.start();
-
+    var t0 = try std.time.Timer.start();
+    // ----------------------------------------------------------
     const task1_limit = 25;
     const task2_limit = 1_000_000_000;
     const task3_limit = 10_000_000_000;
-    const T = AutoSieveType(task3_limit * 10); // allow some leeway
-
-    const stdout = io.getStdOut().writer();
-    try stdout.writeAll("Primesieve Ormiston triples\n\n");
-
-    // -------------------------
-    // allocator for the prime number generator within
-    // the triple generator
-    var gpa = heap.GeneralPurposeAllocator(.{}){};
-    defer _ = gpa.deinit();
-    const allocator = gpa.allocator();
-
-    var generator = try OrmistonTripleGenerator(T).init(allocator);
+    // ----------------------------------------------------------
+    const writer = std.io.getStdOut().writer();
+    try writer.writeAll("Primesieve Ormiston triples\n\n");
+    // ----------------------------------------------------------
+    var generator = try OrmistonTripleGenerator.init();
     defer generator.deinit();
-
     // --------------------------------------------------- task 1
-    try stdout.writeAll("Smallest members of first 25 Ormiston triples:\n");
+    try writer.writeAll("Smallest members of first 25 Ormiston triples:\n");
     var count: usize = 0;
     while (count < task1_limit) : (count += 1) {
         const primes = try generator.next();
-        try stdout.print("{d}{c}", .{ primes[0], @as(u8, if ((count + 1) % 5 == 0) '\n' else ' ') });
+        try writer.print("{d}{c}", .{ primes[0], @as(u8, if ((count + 1) % 5 == 0) '\n' else ' ') });
     }
     // ---------------------------------------------- tasks 2 & 3
-    var limit: T = task2_limit;
+    var limit: u64 = task2_limit;
     while (limit <= task3_limit) : (count += 1) {
         const primes = try generator.next();
         if (primes[2] > limit) {
-            try stdout.print("\nNumber of Ormiston triples < {d}: {d}\n", .{ limit, count });
+            try writer.print("\nNumber of Ormiston triples < {d}: {d}\n", .{ limit, count });
             limit *= 10;
         }
     }
-    // -------------------------
-    try stdout.print("Processed in {}\n", .{fmt.fmtDuration(t0.read())});
+    // ----------------------------------------------------------
+    try writer.print("Processed in {}\n", .{std.fmt.fmtDuration(t0.read())});
 }
 
 const OrmistonTripleGeneratorError = error{
-    PrimeGenNull,
+    PrimesieveError,
 };
 
-fn OrmistonTripleGenerator(T: type) type {
-    if (@typeInfo(T) != .int or @typeInfo(T).int.signedness != .unsigned)
-        @compileError("OrmistonTripleGenerator requires an unsigned integer, found " ++ @typeName(T));
+const OrmistonTripleGenerator = struct {
+    const Self = @This();
 
-    return struct {
-        const Self = @This();
+    it: ps.primesieve_iterator,
+    prime1: u64,
+    prime2: u64,
 
-        prime_iterator: PrimeGen(T),
-        prime1: T,
-        prime2: T,
+    fn init() !Self {
+        var it: ps.primesieve_iterator = undefined;
+        ps.primesieve_init(&it);
 
-        fn init(allocator: mem.Allocator) !Self {
-            var it = PrimeGen(T).init(allocator);
-
-            var primes: [2]T = undefined;
-            for (&primes) |*prime| {
-                if (try it.next()) |p|
-                    prime.* = p
-                else
-                    return OrmistonTripleGeneratorError.PrimeGenNull;
-            }
-            return Self{
-                .prime_iterator = it,
-                .prime1 = primes[0],
-                .prime2 = primes[1],
-            };
+        var primes: [2]u64 = undefined;
+        for (&primes) |*prime| {
+            prime.* = ps.primesieve_next_prime(&it);
+            if (it.is_error != 0 or prime.* == ps.PRIMESIEVE_ERROR)
+                return OrmistonTripleGeneratorError.PrimesieveError;
         }
-        fn deinit(self: *Self) void {
-            self.prime_iterator.deinit();
+        return Self{
+            .it = it,
+            .prime1 = primes[0],
+            .prime2 = primes[1],
+        };
+    }
+    fn deinit(self: *Self) void {
+        defer ps.primesieve_free_iterator(&self.it);
+    }
+    fn next(self: *Self) ![3]u64 {
+        while (true) {
+            const prime = ps.primesieve_next_prime(&self.it);
+            if (self.it.is_error != 0 or prime == ps.PRIMESIEVE_ERROR)
+                return OrmistonTripleGeneratorError.PrimesieveError;
+
+            const prime0 = self.prime1;
+            self.prime1 = self.prime2;
+            self.prime2 = prime;
+
+            // Simple short-circuit
+            // - to be a triple they must differ by 18.
+            // https://oeis.org/A072274
+            if ((self.prime1 - prime0) % 18 != 0)
+                continue;
+            if ((self.prime2 - self.prime1) % 18 != 0)
+                continue;
+
+            // Calculating signatures locally here is
+            // faster than maintaining variables at
+            // struct scope in Self
+            const signature = calcSignature(prime0);
+            if (signature != calcSignature(self.prime1))
+                continue;
+            if (signature != calcSignature(self.prime2))
+                continue;
+
+            return .{ prime0, self.prime1, self.prime2 };
         }
-
-        fn next(self: *Self) ![3]u64 {
-            while (true) {
-                if (try self.prime_iterator.next()) |prime| {
-                    const prime0 = self.prime1;
-                    self.prime1 = self.prime2;
-                    self.prime2 = prime;
-
-                    // Simple short-circuit
-                    // - to be a triple they must differ by 18.
-                    // https://oeis.org/A072274
-                    if ((self.prime1 - prime0) % 18 != 0)
-                        continue;
-                    if ((self.prime2 - self.prime1) % 18 != 0)
-                        continue;
-
-                    // Calculating signatures locally here is
-                    // faster than maintaining variables at
-                    // struct scope in Self
-                    const signature = calcSignature(T, prime0);
-                    if (signature != calcSignature(T, self.prime1))
-                        continue;
-                    if (signature != calcSignature(T, self.prime2))
-                        continue;
-
-                    return .{ prime0, self.prime1, self.prime2 };
-                } else {
-                    return OrmistonTripleGeneratorError.PrimeGenNull;
-                }
-            }
-        }
-    };
-}
+    }
+};
 
 /// Same digits different order will give the same signature.
 /// Count of each digit in 'n_' packed into an integer.
 /// 60 bits (10 decimal digits 0 to 9 inclusive, 6 bits per digit)
 /// - for a number with up to 63 identical decimal digits.
-fn calcSignature(T: type, n_: T) u60 {
+fn calcSignature(n_: anytype) u60 {
+    const T = @TypeOf(n_);
     if (@typeInfo(T) != .int or @typeInfo(T).int.signedness != .unsigned)
         @compileError("calcSignature requires an unsigned integer, found " ++ @typeName(T));
 
