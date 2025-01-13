e44be2d342f7d1c74f9051648aebc416df3dd805
stephen <>
2025-01-13 23:14:30 +0000

    refactor: Recaman%27s_sequence.zig as a "Translation of Go" using Zig's std.ArrayList instead of a guesstimate for array length.
    FossilOrigin-Name: 21d04b99ad9f3413d8dba812d9f3c10f153074a6d305fa53f368344cfabb66eb

--- Recaman%27s_sequence.zig
+++ Recaman%27s_sequence.zig
@@ -1,59 +1,55 @@
 // https://rosettacode.org/wiki/Recaman%27s_sequence
-// Translation of C
+// Translation of Go
 const std = @import("std");
 
 pub fn main() !void {
-    const stdout = std.io.getStdOut().writer();
+    const writer = std.io.getStdOut().writer();
 
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    var gpa: std.heap.GeneralPurposeAllocator(.{}) = .init;
     defer _ = gpa.deinit();
     const allocator = gpa.allocator();
 
-    var used = std.AutoArrayHashMap(i64, void).init(allocator);
-    var used1000 = std.AutoArrayHashMap(i64, void).init(allocator);
+    // arraylist can be initialised with capacity as the answers are known...
+    var a = try std.ArrayList(isize).initCapacity(allocator, 400_000);
+    defer a.deinit();
+
+    var used = std.AutoHashMap(isize, void).init(allocator);
     defer used.deinit();
+    try used.ensureTotalCapacity(1001);
+
+    var used1000 = std.AutoHashMap(isize, void).init(allocator);
     defer used1000.deinit();
+    try used1000.ensureTotalCapacity(1001);
 
-    var a = try allocator.alloc(i64, 400_000);
-    defer allocator.free(a);
-    a[0] = 0;
-
+    try a.append(0);
     try used.put(0, {});
     try used1000.put(0, {});
 
-    var k: usize = 0;
     var n: usize = 1;
-    var found_dup: bool = false;
+    var found_dup = false;
 
-    while (n <= 15 or !found_dup or k < 1001) : (n += 1) {
-        var next = a[n - 1] - @as(i64, @intCast(n));
+    while (n <= 15 or !found_dup or used1000.count() < 1001) : (n += 1) {
+        var next = a.items[n - 1] - @as(isize, @intCast(n));
         if (next < 1 or used.contains(next))
-            next += 2 * @as(i64, @intCast(n));
+            next += 2 * @as(isize, @intCast(n));
 
         const already_used = used.contains(next);
-        a[n] = next;
-
+        try a.append(next);
         if (!already_used) {
             try used.put(next, {});
-            if (next >= 0 and next <= 1000)
+            if (next <= 1000)
                 try used1000.put(next, {});
         }
 
-        if (n == 14) {
-            try stdout.writeAll("The first 15 terms of the Recaman's sequence are: ");
-            try stdout.writeAll("[");
-            for (0..15) |i|
-                try stdout.print("{d} ", .{a[i]});
-            try stdout.writeAll("]\n");
-        }
+        if (n == 14)
+            try writer.print("The first 15 terms of the Recaman's sequence are: {any}\n", .{a.items});
 
         if (!found_dup and already_used) {
-            try stdout.print("The first duplicated term is a[{d}] = {d}\n", .{ n, next });
+            try writer.print("The first duplicated term is a[{d}] = {d}\n", .{ n, next });
             found_dup = true;
         }
 
-        k = used1000.count();
-        if (k == 1001)
-            try stdout.print("Terms up to a[{d}] are needed to generate 0 to 1000\n", .{n});
+        if (used1000.count() == 1001)
+            try writer.print("Terms up to a[{d}] are needed to generate 0 to 1000\n", .{n});
     }
 }
