3c81ce1b185d6a2ee7a7192fc3193cc230c3413d
stephen <>
2025-09-19 22:10:11 +0000

    update to Zig 0.15.1
    FossilOrigin-Name: ae1a5744835427d17f9b87f180361a82320fb0c5fd9d1264e92ce9a96b097fe6

--- A%2BB.zig
+++ A%2BB.zig
@@ -1,21 +1,36 @@
 // https://rosettacode.org/wiki/A%2BB
+// {{works with|Zig|0.15.1}}
 // Copied from rosettacode
 const std = @import("std");
 
 pub fn main() !void {
-    var buf: [1024]u8 = undefined;
-    const reader = std.io.getStdIn().reader();
-    const input = try reader.readUntilDelimiter(&buf, '\n');
-    const text = std.mem.trimRight(u8, input, "\r\n");
+    var stdout_buffer: [1024]u8 = undefined;
+    var stdin_buffer: [1024]u8 = undefined;
+    var stdout_writer = std.fs.File.stdout().writer(&stdout_buffer);
+    var stdin_reader = std.fs.File.stdin().reader(&stdin_buffer);
+    const stdout = &stdout_writer.interface;
+    const stdin = &stdin_reader.interface;
+    // for the stdin input text
+    var buffer1: [1024]u8 = undefined;
+    var w: std.Io.Writer = .fixed(&buffer1);
+    // for the tokenized input
+    var buffer2: [2][]const u8 = undefined;
+    var values: std.ArrayList([]const u8) = .initBuffer(&buffer2);
+    // ----------------------------------------------------
+    try stdout.writeAll("Enter two numbers: ");
+    try stdout.flush();
 
-    var values = try std.BoundedArray([]const u8, 2).init(0);
+    _ = try stdin.streamDelimiter(&w, '\n');
+    const text = std.mem.trimRight(u8, w.buffered(), "\r");
+
     var it = std.mem.tokenizeScalar(u8, text, ' ');
     while (it.next()) |value|
-        try values.append(value);
+        try values.appendBounded(value);
 
-    const a = try std.fmt.parseInt(u64, values.get(0), 10);
-    const b = try std.fmt.parseInt(u64, values.get(1), 10);
+    const a = try std.fmt.parseInt(u64, values.items[0], 10);
+    const b = try std.fmt.parseInt(u64, values.items[1], 10);
 
-    const stdout = std.io.getStdOut().writer();
-    try stdout.print("{d}\n", .{a + b});
+    try stdout.print("Sum = {d}\n", .{a + b});
+    // ----------------------------------------------------
+    try stdout.flush();
 }
