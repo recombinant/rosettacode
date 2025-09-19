a9029b3cdff4d68bb48317414cd20b5031566d5b
stephen <>
2025-09-19 22:07:25 +0000

    update to Zig 0.15.1
    FossilOrigin-Name: 9c6bc892cdca99704a90a768d32017701d391758ff913b118db9109df688d425

--- Odd_words(2).zig
+++ Odd_words(2).zig
@@ -1,9 +1,9 @@
 // https://rosettacode.org/wiki/Odd_words
+// {{works with|Zig|0.15.1}}
 const std = @import("std");
-const File = std.fs.File;
 
 pub fn main() !void {
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    var gpa: std.heap.GeneralPurposeAllocator(.{}) = .init;
     defer _ = gpa.deinit();
     const allocator = gpa.allocator();
 
@@ -13,17 +13,38 @@ pub fn main() !void {
         while (it.next()) |key| allocator.free(key.*);
         word_set.deinit();
     }
-    try word_set.ensureTotalCapacity(26_000);
 
-    var file: File = try std.fs.cwd().openFile("data/unixdict.txt", .{});
+    var file: std.fs.File = try std.fs.cwd().openFile("data/unixdict.txt", .{});
     defer file.close();
 
-    var buf_reader = std.io.bufferedReader(file.reader());
-    var in_stream = buf_reader.reader();
+    var buffer1: [4096]u8 = undefined;
+    var file_reader = file.reader(&buffer1);
+    var r = &file_reader.interface;
 
-    var max_len: usize = 0;
-    var buf: [1024]u8 = undefined;
-    while (try in_stream.readUntilDelimiterOrEof(&buf, '\n')) |word| {
+    // assume words are <= 64 bytes in length
+    var buffer2: [64]u8 = undefined;
+    var w = std.Io.Writer.fixed(&buffer2);
+
+    // ---------------------------------------------------
+    // count the words for `word_set` capacity
+    const count = try getWordCount(r, &w);
+    // with the exact count no realloc will be necessary
+    try word_set.ensureTotalCapacity(count);
+    std.debug.print("dictionary usable word count = {}\n", .{count});
+
+    try file_reader.seekTo(0); // rewind
+    _ = w.consumeAll(); // reset `w` writer
+    // ---------------------------------------------------
+    var max_len: usize = 0; // for buffer allocation
+    // put the words into `word_set`
+    while (0 != try r.streamDelimiterEnding(&w, '\n')) : (_ = w.consumeAll()) {
+        const word = w.buffered();
+        // consume the '\n' (or catch eof)
+        _ = r.takeByte() catch |err| switch (err) {
+            error.EndOfStream => {},
+            else => |e| return e,
+        };
+
         if (word.len > 4) {
             const m = try allocator.dupe(u8, word);
             try word_set.putNoClobber(m, {});
@@ -38,43 +59,49 @@ pub fn main() !void {
         odd_word_set.deinit();
     }
 
-    var even_word_set = std.StringHashMap(void).init(allocator);
-    defer {
-        var it = even_word_set.keyIterator();
-        while (it.next()) |key| allocator.free(key.*);
-        even_word_set.deinit();
-    }
+    var buffer3 = try allocator.alloc(u8, max_len);
+    defer allocator.free(buffer3);
 
-    var buffer1 = try allocator.alloc(u8, max_len);
-    var buffer2 = try allocator.alloc(u8, max_len);
-    defer allocator.free(buffer1);
-    defer allocator.free(buffer2);
-
-    var it = word_set.keyIterator();
-    while (it.next()) |word| {
+    var it1 = word_set.keyIterator();
+    while (it1.next()) |word| {
         if (word.len > 8) {
-            var end: usize = undefined;
-            for (word.*, 0..) |letter, i| {
-                end = i / 2;
-                if (i & 1 == 0) {
-                    buffer1[end] = letter;
-                } else {
-                    buffer2[end] = letter;
-                }
-            }
-            const odd_word = buffer1[0 .. end + 1];
+            var len: usize = 0;
+            var it2 = std.mem.window(u8, word.*, 1, 2);
+            while (it2.next()) |letter| : (len += 1)
+                buffer3[len] = letter[0];
+
+            const odd_word = buffer3[0..len];
             if (word_set.contains(odd_word)) {
                 if (!odd_word_set.contains(odd_word))
                     try odd_word_set.putNoClobber(try allocator.dupe(u8, odd_word), {});
             }
-            const even_word = buffer2[0 .. end + 1];
-            if (word_set.contains(even_word)) {
-                if (!even_word_set.contains(even_word))
-                    try even_word_set.putNoClobber(try allocator.dupe(u8, even_word), {});
-            }
         }
     }
-    it = odd_word_set.keyIterator();
-    while (it.next()) |word|
-        try std.io.getStdOut().writer().print("{s}\n", .{word.*});
+
+    var stdout_buffer: [1024]u8 = undefined;
+    var stdout_writer = std.fs.File.stdout().writer(&stdout_buffer);
+    const stdout = &stdout_writer.interface;
+
+    it1 = odd_word_set.keyIterator();
+    while (it1.next()) |word|
+        try stdout.print("{s}\n", .{word.*});
+
+    try stdout.flush();
+}
+
+// Count all the words with a length greater than 4
+// (minimum length for a odd word).
+fn getWordCount(r: *std.Io.Reader, w: *std.Io.Writer) !u32 {
+    var count: u32 = 1;
+    while (0 != try r.streamDelimiterEnding(w, '\n')) : (_ = w.consumeAll()) {
+        // consume the '\n' (or catch eof)
+        _ = r.takeByte() catch |err| switch (err) {
+            error.EndOfStream => {},
+            else => |e| return e,
+        };
+        const word = w.buffered();
+        if (word.len > 4)
+            count += 1;
+    }
+    return count;
 }
