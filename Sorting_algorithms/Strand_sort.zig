2536b46f3ed04743777281afd328ce9691850829
stephen <>
2025-09-22 13:37:35 +0000

    update to Zig 0.15.1
    FossilOrigin-Name: fcadaa0560c3f72e6b9c03d436b267f4102bf1fb9d3f03dcd0c29231ef7c6ae2

--- Sorting_algorithms/Strand_sort.zig
+++ Sorting_algorithms/Strand_sort.zig
@@ -1,46 +1,60 @@
 // https://rosettacode.org/wiki/Sorting_algorithms/Strand_sort
+// {{works with|Zig|0.15.1}}
 const std = @import("std");
 
-const List = std.DoublyLinkedList(u16);
-const NodePool = std.heap.MemoryPoolExtra(List.Node, .{});
+const List = std.DoublyLinkedList;
+
+const Element = struct {
+    node: List.Node = .{},
+    data: u16,
+};
+
+const NodePool = std.heap.MemoryPoolExtra(Element, .{});
 
 pub fn main() !void {
     var node_pool = NodePool.init(std.heap.page_allocator);
     defer node_pool.deinit();
 
-    const writer = std.io.getStdOut().writer();
+    var stdout_buffer: [1024]u8 = undefined;
+    var stdout_writer = std.fs.File.stdout().writer(&stdout_buffer);
+    const stdout = &stdout_writer.interface;
 
     // Create a new linked list of integers
-    var list = List{};
+    var list: List = .{};
 
     // Add the following integers to the linked list
     const integers = [_]u16{ 5, 1, 4, 2, 0, 9, 6, 3, 8, 7 };
     for (integers) |int| {
-        const node = try node_pool.create();
-        node.* = List.Node{
-            .data = int,
-        };
-        list.append(node);
+        const element = try node_pool.create();
+        element.* = .{ .data = int };
+        list.append(&element.node);
     }
 
     StrandSort.sort(&list);
 
     // Print out the solution list
     var it = list.first;
-    while (it) |node| : (it = node.next)
-        try writer.print("{d}\n", .{node.data});
+    while (it) |node| : (it = node.next) {
+        const element: *Element = @fieldParentPtr("node", node);
+        try stdout.print("{d} ", .{element.data});
+    }
+    try stdout.writeByte('\n');
+
+    try stdout.flush();
 }
 
 const StrandSort = struct {
     fn sort(list: *List) void {
-        var leftover = List{};
-        var sorted1 = List{};
+        var leftover: List = .{};
+        var sorted1: List = .{};
 
-        var result: List = List{};
-        while (list.len != 0) {
+        var result: List = .{};
+        while (list.first != null) {
             sorted1.append(list.popFirst().?);
             while (list.popFirst()) |node| {
-                if (sorted1.last.?.data <= node.data)
+                const element: *Element = @fieldParentPtr("node", node);
+                const element1: *Element = @fieldParentPtr("node", sorted1.last.?);
+                if (element1.data <= element.data)
                     sorted1.append(node)
                 else
                     leftover.append(node);
@@ -53,9 +67,11 @@ const StrandSort = struct {
     }
 
     fn merge(left: *List, right: *List) List {
-        var result = List{};
-        while (left.len != 0 and right.len != 0) {
-            if (left.first.?.data <= right.first.?.data)
+        var result: List = .{};
+        while (left.first != null and right.first != null) {
+            const element_left: *Element = @fieldParentPtr("node", left.first.?);
+            const element_right: *Element = @fieldParentPtr("node", right.first.?);
+            if (element_left.data <= element_right.data)
                 result.append(left.popFirst().?)
             else
                 result.append(right.popFirst().?);
--- Sum_and_product_puzzle.zig
+++ Sum_and_product_puzzle.zig
@@ -1,199 +1,195 @@
 // https://rosettacode.org/wiki/Sum_and_product_puzzle
-// Translation of C
+// {{works with|Zig|0.15.1}}
+// {{trans|C}}
 const std = @import("std");
-const heap = std.heap;
-const mem = std.mem;
 
-const Pair = struct { x: u64, y: u64 };
-const L = std.SinglyLinkedList(Pair);
-const Pool = heap.MemoryPoolExtra(L.Node, .{});
+const List = std.SinglyLinkedList;
+const Item = struct {
+    node: List.Node = .{},
+    x: u64,
+    y: u64,
+};
+const Pool = std.heap.MemoryPoolExtra(Item, .{});
 
 pub fn main() !void {
     // ----------------------------------------------------
-    var pool = Pool.init(heap.page_allocator);
+    var pool: Pool = .init(std.heap.page_allocator);
     defer pool.deinit();
     // ----------------------------------------------------
-    var gpa = heap.GeneralPurposeAllocator(.{}){};
+    var gpa: std.heap.DebugAllocator(.{}) = .init;
     defer _ = gpa.deinit();
     const allocator = gpa.allocator();
     // ----------------------------------------------------
-    const stdout = std.io.getStdOut().writer();
+    var stdout_buffer: [1024]u8 = undefined;
+    var stdout_writer = std.fs.File.stdout().writer(&stdout_buffer);
+    const stdout = &stdout_writer.interface;
     // ----------------------------------------------------
-    var candidates = try init(&pool);
+    var candidates = try setup(&pool);
     defer deinit(&pool, &candidates);
-    try printCount(stdout, candidates);
+    try printCount(candidates, stdout);
 
     try statement1(allocator, &pool, &candidates);
-    try printCount(stdout, candidates);
+    try printCount(candidates, stdout);
 
     try statement2(allocator, &pool, &candidates);
-    try printCount(stdout, candidates);
+    try printCount(candidates, stdout);
 
     try statement3(allocator, &pool, &candidates);
-    try printCount(stdout, candidates);
+    try printCount(candidates, stdout);
 
-    try printList(stdout, candidates);
+    try printList(candidates, stdout);
+
+    try stdout.flush();
 }
 
-fn printList(writer: anytype, list: L) !void {
+fn printList(list: List, w: *std.Io.Writer) !void {
     var node_ = list.first;
     while (node_) |node| : (node_ = node.next) {
-        const pair = node.data;
-        const sum = pair.x + pair.y;
-        const product = pair.x * pair.y;
-        try writer.print("[{}, {}] S={} P={}\n", .{ pair.x, pair.y, sum, product });
+        const item: *Item = @fieldParentPtr("node", node);
+        const sum = item.x + item.y;
+        const product = item.x * item.y;
+        try w.print("[{}, {}] S={} P={}\n", .{ item.x, item.y, sum, product });
     }
 }
 
-fn printCount(writer: anytype, list: L) !void {
+fn printCount(list: List, w: *std.Io.Writer) !void {
     const len = list.len();
     try switch (len) {
-        0 => writer.writeAll("no candidates\n"),
-        1 => writer.writeAll("one candidate\n"),
-        else => writer.print("{} candidates\n", .{len}),
+        0 => w.writeAll("no candidates\n"),
+        1 => w.writeAll("one candidate\n"),
+        else => w.print("{} candidates\n", .{len}),
     };
 }
 
-fn init(pool: *Pool) !L {
-    var list = L{};
-
-    const max_sum = 100;
+fn setup(pool: *Pool) !List {
+    var list = List{};
 
     // numbers must be greater than 1
-    for (2..max_sum / 2 - 1) |x| {
+    for (2..99) |x| {
         // numbers must be unique, and sum no more than 100
-        for (x + 1..max_sum - 2) |y| {
-            if (x + y <= max_sum) {
-                const node = try pool.create();
-                node.* = L.Node{ .data = .{ .x = x, .y = y } };
-                list.prepend(node);
+        for (x + 1..99) |y| {
+            if (x + y <= 100) {
+                const item = try pool.create();
+                item.* = .{ .x = x, .y = y };
+                list.prepend(&item.node);
             }
         }
     }
     return list;
 }
 
-fn deinit(pool: *Pool, list: *L) void {
-    while (list.popFirst()) |node|
-        pool.destroy(node);
-}
-
-/// May invalidate any exising pointers into list.
-fn removeBySum(pool: *Pool, list: *L, sum: u64) void {
-    var node_ = list.first;
-    while (node_) |node| {
-        node_ = node.next; // move to next before possibly removing node
-        const pair = node.data;
-        if (pair.x + pair.y == sum) {
-            list.remove(node);
-            pool.destroy(node);
-        }
+fn deinit(pool: *Pool, list: *List) void {
+    while (list.popFirst()) |node| {
+        const item: *Item = @fieldParentPtr("node", node);
+        pool.destroy(item);
     }
 }
 
 /// May invalidate any exising pointers into list.
-fn removeByProduct(pool: *Pool, list: *L, product: u64) void {
-    var node_ = list.first;
-    while (node_) |node| {
-        node_ = node.next; // move to next before possibly removing node
-        const pair = node.data;
-        if (pair.x * pair.y == product) {
+fn removeBySum(pool: *Pool, list: *List, sum: u64) void {
+    var it = list.first;
+    while (it) |node| {
+        const item: *Item = @fieldParentPtr("node", node);
+        if (item.x + item.y == sum) {
             list.remove(node);
-            pool.destroy(node);
+            pool.destroy(item);
+            it = list.first;
+        } else {
+            it = node.next;
         }
     }
 }
 
-/// product vs product frequency
-fn statement1(allocator: mem.Allocator, pool: *Pool, list: *L) !void {
-    var pair_counts = std.AutoArrayHashMap(u64, u32).init(allocator);
-    defer pair_counts.deinit();
-
-    {
-        var node_ = list.first;
-        while (node_) |node| {
-            node_ = node.next;
-            const pair = node.data;
-            const product = pair.x * pair.y;
-            const gop = try pair_counts.getOrPutValue(product, 1);
-            if (gop.found_existing)
-                gop.value_ptr.* += 1;
-        }
-    }
-    {
-        var node_ = list.first;
-        while (node_) |node| {
-            node_ = node.next;
-            const pair = node.data;
-            const product = pair.x * pair.y;
-            if (pair_counts.get(product).? == 1) {
-                removeBySum(pool, list, pair.x + pair.y);
-                node_ = list.first; // because `node_` may be invalidated
-            }
+/// May invalidate any exising pointers into list.
+fn removeByProduct(pool: *Pool, list: *List, product: u64) void {
+    var it = list.first;
+    while (it) |node| {
+        const item: *Item = @fieldParentPtr("node", node);
+        if (item.x * item.y == product) {
+            list.remove(node);
+            pool.destroy(item);
+            it = list.first;
+        } else {
+            it = node.next;
         }
     }
 }
 
-/// product vs product frequency
-fn statement2(allocator: mem.Allocator, pool: *Pool, list: *L) !void {
-    var pair_counts = std.AutoArrayHashMap(u64, u32).init(allocator);
-    defer pair_counts.deinit();
+fn statement1(allocator: std.mem.Allocator, pool: *Pool, list: *List) !void {
+    var pair_counts: std.AutoArrayHashMapUnmanaged(u64, u8) = .empty;
+    defer pair_counts.deinit(allocator);
 
-    {
-        var node_ = list.first;
-        while (node_) |node| {
-            node_ = node.next;
-            const pair = node.data;
-            const product = pair.x * pair.y;
-            const gop = try pair_counts.getOrPutValue(product, 1);
-            if (gop.found_existing)
-                gop.value_ptr.* += 1;
-        }
+    var it = list.first;
+    while (it) |node| : (it = node.next) {
+        const item: *Item = @fieldParentPtr("node", node);
+        const product = item.x * item.y;
+        const gop = try pair_counts.getOrPutValue(allocator, product, 1);
+        if (gop.found_existing)
+            gop.value_ptr.* += 1;
     }
-    {
-        var node_ = list.first;
-        while (node_) |node| {
-            node_ = node.next;
-            const pair = node.data;
-            const product = pair.x * pair.y;
-            if (pair_counts.get(product).? > 1) {
-                list.remove(node);
-                pool.destroy(node);
-                removeByProduct(pool, list, product);
-                node_ = list.first; // because `node_` may be invalidated
-            }
+
+    it = list.first;
+    while (it) |node| {
+        const item: *Item = @fieldParentPtr("node", node);
+        const product = item.x * item.y;
+        if (pair_counts.get(product).? == 1) {
+            removeBySum(pool, list, item.x + item.y);
+            it = list.first;
+        } else {
+            it = node.next;
         }
     }
 }
 
-/// sum vs sum frequency
-fn statement3(allocator: mem.Allocator, pool: *Pool, list: *L) !void {
-    var pair_counts = std.AutoArrayHashMap(u64, u32).init(allocator);
-    defer pair_counts.deinit();
+fn statement2(allocator: std.mem.Allocator, pool: *Pool, list: *List) !void {
+    var pair_counts: std.AutoArrayHashMapUnmanaged(u64, u8) = .empty;
+    defer pair_counts.deinit(allocator);
 
-    {
-        var node_ = list.first;
-        while (node_) |node| {
-            node_ = node.next;
-            const pair = node.data;
-            const sum = pair.x + pair.y;
-            const gop = try pair_counts.getOrPutValue(sum, 1);
-            if (gop.found_existing)
-                gop.value_ptr.* += 1;
+    var it = list.first;
+    while (it) |node| : (it = node.next) {
+        const item: *Item = @fieldParentPtr("node", node);
+        const product = item.x * item.y;
+        const gop = try pair_counts.getOrPutValue(allocator, product, 1);
+        if (gop.found_existing)
+            gop.value_ptr.* += 1;
+    }
+
+    it = list.first;
+    while (it) |node| {
+        const item: *Item = @fieldParentPtr("node", node);
+        const product = item.x * item.y;
+        if (pair_counts.get(product).? > 1) {
+            list.remove(node);
+            pool.destroy(item);
+            removeByProduct(pool, list, product);
+            it = list.first;
+        } else {
+            it = node.next;
         }
     }
-    {
-        var node_ = list.first;
-        while (node_) |node| {
-            node_ = node.next;
-            const pair = node.data;
-            const sum = pair.x + pair.y;
-            if (pair_counts.get(sum).? > 1) {
-                list.remove(node);
-                pool.destroy(node);
-                removeBySum(pool, list, sum);
-                node_ = list.first; // because `node_` may be invalidated
-            }
+}
+
+fn statement3(allocator: std.mem.Allocator, pool: *Pool, list: *List) !void {
+    var unique = try allocator.alloc(u8, 100);
+    defer allocator.free(unique);
+    @memset(unique, 0);
+
+    var it = list.first;
+    while (it) |node| : (it = node.next) {
+        const item: *Item = @fieldParentPtr("node", node);
+        const sum = item.x + item.y;
+        unique[sum] += 1;
+    }
+
+    it = list.first;
+    while (it) |node| {
+        const item: *Item = @fieldParentPtr("node", node);
+        const sum = item.x + item.y;
+        if (unique[sum] > 1) {
+            removeBySum(pool, list, sum);
+            it = list.first;
+        } else {
+            it = node.next;
         }
     }
 }
